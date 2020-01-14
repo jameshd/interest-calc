e9486ae1dfd391e53322f7a1492721ea1f79648d
jameshd <james.hattersley-dykes@shopdirect.com>
2020-01-14 09:29:10 +0000

    :zap: removed interest rates file.

--- src/bank-account.js
+++ src/bank-account.js
@@ -1,21 +1,19 @@
-import { BASIC_RATE, MID_RATE, HIGH_RATE } from "./interest-rates";
-
 export const calculateInterest = (balance, customerTenure) => {
   const isLoyalCustomer = customerTenure >= 5;
-  let rate = BASIC_RATE;
+  let rate;
 
   if (balance == 0) return 0;
 
-  if (balance > 0 && balance <= 1000) {
-    rate = BASIC_RATE;
+  if (balance <= 1000) {
+    rate = 0.01;
   }
 
-  if (balance > 1000 && balance <= 5000) {
-    rate = MID_RATE;
+  if (balance > 1000) {
+    rate = 0.02;
   }
 
   if (balance > 5000) {
-    rate = HIGH_RATE;
+    rate = 0.03;
   }
 
   return isLoyalCustomer ? balance * (rate * 2) : balance * rate;
--- src/interest-rates.js
+++
@@ -1,3 +0,0 @@
-export const BASIC_RATE = 0.01;
-export const MID_RATE = 0.02;
-export const HIGH_RATE = 0.03
